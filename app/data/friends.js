fd59e41dbddc9cd5f9140419d1a46b01e7e123b2
Mona Lisa <jb47472@hotmail.com>
2019-04-20 16:13:05 -0400

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,244 @@
+var friendsArray = [
+    {
+        "name": "Ted Danson",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Mariah Carey",
+        "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+        "scores": [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "Nick Jonas",
+        "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Dolly Parton",
+        "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        "scores": [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        "name": "Alex Trabec",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+        "scores": [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        "name": "Kobe Byrant",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+        "scores": [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    },
+    {
+        "name": "Mike Jobs",
+        "photo": "https://images.app.goo.gl/5zgbwPBYLe9ojPbs5",
+        "scores": [
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3"
+        ]
+    },
+    {
+        "name": "Terry Bradshaw",
+        "photo": "facebook.com",
+        "scores": [
+            "2",
+            "4",
+            "5",
+            "2",
+            "3",
+            "3",
+            "4",
+            "3",
+            "2",
+            "4"
+        ]
+    },
+    {
+        "name": "Missy Elliot",
+        "photo": "i",
+        "scores": [
+            "4",
+            "2",
+            "2",
+            "1",
+            "4",
+            "3",
+            "3",
+            "4",
+            "1",
+            "4"
+        ]
+    },
+    {
+        "name": "Dr Dre",
+        "photo": "qergewr",
+        "scores": [
+            "3",
+            "3",
+            "3",
+            "2",
+            "1",
+            "4",
+            "3",
+            "3",
+            "3",
+            "1"
+        ]
+    },
+    {
+        "name": "Freddie Mercury",
+        "photo": "http:/",
+        "scores": [
+            "4",
+            "4",
+            "5",
+            "3",
+            "3",
+            "2",
+            "2",
+            "3",
+            "3",
+            "1"
+        ]
+    },
+    {
+        "name": "Eminem",
+        "photo": "https://blog.sqlauthority.com/wp-content/uploads/2016/06/banner-1500-190.jpg",
+        "scores": [
+            "2",
+            "4",
+            "2",
+            "4",
+            "2",
+            "2",
+            "2",
+            "5",
+            "3",
+            "3"
+        ]
+    },
+    {
+        "name": "Prince Lord Byron",
+        "photo": "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/natalia-vodianova-1518453548.jpg?crop=1xw:1xh;center,top&resize=480:*",
+        "scores": [
+            "2",
+            "2",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "3",
+            "2",
+            "2"
+        ]
+    },
+    {
+        "name": "Miley Virus",
+        "photo": "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.gannett-cdn.com%2F-mm-%2F5f191823ab8990b019840f4061d41ea2a509ff64%2Fc%3D0-42-3262-1885%2Flocal%2F-%2Fmedia%2F2018%2F03%2F14%2FUSATODAY%2FUSATODAY%2F636566518724775036-AP-Music-Miley-Cyrus-Lawsuit.jpg%3Fwidth%3D3200%26height%3D1680%26fit%3Dcrop&imgrefurl=https%3A%2F%2Fwww.usatoday.com%2Fstory%2Flife%2Fentertainthis%2F2018%2F11%2F24%2Fmiley-cyrus-celebrates-birthday-family-after-losing-home-fire%2F2102102002%2F&docid=w7XqTHkIWcUINM&tbnid=LgVd0ZPj_pIfpM%3A&vet=10ahUKEwjA282gvd_hAhUHEqwKHQnyBSYQMwhrKAIwAg..i&w=3200&h=1680&bih=820&biw=1669&q=miley%20cyrus&ved=0ahUKEwjA282gvd_hAhUHEqwKHQnyBSYQMwhrKAIwAg&iact=mrc&uact=8",
+        "scores": [
+            "2",
+            "2",
+            "3",
+            "4",
+            "5",
+            "2",
+            "3",
+            "4",
+            "1",
+            "3"
+        ]
+    },
+    {
+        "name": "Tim-O-Thee",
+        "photo": "https://imgur.com/gallery/Zfpos",
+        "scores": [
+            "2",
+            "3",
+            "5",
+            "2",
+            "3",
+            "3",
+            "3",
+            "4",
+            "4",
+            "3"
+        ]
+    }
+]
+
+module.exports = friendsArray;
